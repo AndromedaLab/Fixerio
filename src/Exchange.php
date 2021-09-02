b66294d5ff59fed3c981296a9e34e91bae4f8e4d
Aleksandr Stifanenkov <stifanenkov@llcglobaladvisors.com>
2021-09-02 16:53:45 +0300

    Change error handling from response according to changes in fixer.io API.

--- src/Exchange.php
+++ src/Exchange.php
@@ -239,7 +239,8 @@ class Exchange
             $url .= '&access_key=' . $this->key;
         }
 
-        if ($symbols = $this->symbols) {
+        $symbols = $this->symbols;
+        if ($symbols) {
             $url .= '&symbols=' . implode(',', $symbols);
         }
 
@@ -273,7 +274,9 @@ class Exchange
         }
 
         if ($response['success'] === false) {
-            throw new ResponseException($response['error']['info'], $response['error']['code']);
+            $errorCode = isset($response['error']['code']) ? (int)$response['error']['code'] : 0;
+
+            throw new ResponseException($this->getErrorMessageByCode($errorCode), $errorCode);
         }
 
         if (!is_array($response['rates'])) {
@@ -314,6 +317,36 @@ class Exchange
         }
 
         throw new ResponseException('Response body is malformed.');
+    }
+
+    /**
+     * @param int $code
+     *
+     * @return string
+     */
+    private function getErrorMessageByCode($code)
+    {
+        $errorMessageByCode = [
+            404 => 'The requested resource does not exist.',
+            101 => 'No API Key was specified or an invalid API Key was specified.',
+            103 => 'The requested API endpoint does not exist.',
+            104 => 'The maximum allowed API amount of monthly API requests has been reached.',
+            105 => 'The current subscription plan does not support this API endpoint.',
+            106 => 'The current request did not return any results.',
+            102 => 'The account this API request is coming from is inactive.',
+            201 => 'An invalid base currency has been entered.',
+            202 => 'One or more invalid symbols have been specified.',
+            301 => 'No date has been specified.',
+            302 => 'An invalid date has been specified.',
+            403 => 'No or an invalid amount has been specified.',
+            501 => 'No or an invalid timeframe has been specified.',
+            502 => 'No or an invalid "start_date" has been specified.',
+            503 => 'No or an invalid "end_date" has been specified.',
+            504 => 'An invalid timeframe has been specified.',
+            505 => 'The specified timeframe is too long, exceeding 365 days.',
+        ];
+
+        return isset($errorMessageByCode[$code]) ? $errorMessageByCode[$code] : 'Unknown error.';
     }
 
 }
